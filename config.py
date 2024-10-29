9fd4f0f890bebc678dda79221ab650b9faf6a133
Rebelstar <145977706+Jaygiri07@users.noreply.github.com>
2024-10-29 22:01:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config(object):
     API_HASH = os.environ.get("ba25181c01b50d945748801b6c8b6ecc")
     BOT_TOKEN = os.environ.get("8036810739:AAEWFFuVNtosCRFhPWUCUhOTbzZK_-BKpxk")
     TELEGRAM_API = os.environ.get("26162072")
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
+    OWNER = os.environ.get("6717382350")
+    OWNER_USERNAME = os.environ.get("Stylish_Star2")
+    PASSWORD = os.environ.get("Rebel09")
     DATABASE_URL = os.environ.get("mongodb+srv://j32025026:<db_diTBNy6AvTzCpOYi>@cluster0.mmcgi.mongodb.net/?retryWrites=true&w=majority")
     LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
