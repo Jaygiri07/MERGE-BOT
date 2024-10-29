badc5ffc22fdf2a809bfcbbfd722fb47b2db05eb
Rebelstar <145977706+Jaygiri07@users.noreply.github.com>
2024-10-29 22:03:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     OWNER_USERNAME = os.environ.get("Stylish_Star2")
     PASSWORD = os.environ.get("Rebel09")
     DATABASE_URL = os.environ.get("mongodb+srv://j32025026:<db_diTBNy6AvTzCpOYi>@cluster0.mmcgi.mongodb.net/?retryWrites=true&w=majority")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    LOGCHANNEL = os.environ.get("-1002446968293")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
