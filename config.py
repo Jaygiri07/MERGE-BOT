3e7c267e48c1303e1208cac4f5ebb1016e49709f
Rebelstar <145977706+Jaygiri07@users.noreply.github.com>
2024-10-30 15:50:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     OWNER = os.environ.get("6717382350")
     OWNER_USERNAME = os.environ.get("Stylish_Star2")
     PASSWORD = os.environ.get("Rebel09")
-    DATABASE_URL = os.environ.get("mongodb+srv://j32025026:<db_diTBNy6AvTzCpOYi>@cluster0.mmcgi.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URL = os.environ.get("mongodb+srv://gtachno8:r7fbN5gUFt8fEXe5@cluster0.bjuv8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     LOGCHANNEL = os.environ.get("-1002446968293")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
